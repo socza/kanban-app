cf7800d648ef584e7d1b0c1821b1e030b3ca83dd
Piotr Socińśki <psocinski@gmail.com>
2018-04-10 00:06:43 +0200

    Added reducers

--- client/modules/Lane/LaneReducer.js
+++ client/modules/Lane/LaneReducer.js
@@ -1,5 +1,6 @@
 // Import Actions
 import { CREATE_LANE, UPDATE_LANE, DELETE_LANE } from './LaneActions';
+import { CREATE_NOTE, DELETE_NOTE } from '../Note/NoteActions';
 
 // Initial State
 sconst initialState = [];
@@ -17,6 +18,24 @@ export default function lanes(state = initialState, action) {
 
   	case DELETE_LANE:
   		return state.filter(lane => lane.id !== action.laneId);
+
+  	case CREATE_NOTE:
+  		return state.map(lane => {
+    		if (lane.id === action.laneId) {
+      			const notes = [...lane.notes, action.note.id];
+      			return { ...lane, notes };
+    		}
+    		return lane;
+  		});
+
+  	case DELETE_NOTE:
+		return state.map(lane => {
+			if (lane.id === action.laneId) {
+				const notes = [...lane.notes].filter( note => note.id !== action.noteId);
+				return { ...lane, notes };
+			}
+			return lane;
+		});
 
     default:
       	return state;
